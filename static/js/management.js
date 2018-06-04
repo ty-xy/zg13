e0ae4d70674b2bd250b695388524236b7be375cb
zyc <zg-zyc@zg18.com>
2018-06-04 09:50:05 +0800

    feat:添加日历

--- static/js/management.js
+++ static/js/management.js
@@ -56,6 +56,30 @@
         //         $(this).unbind("mousemove");
         //     })
         // })
+    //初始化日历
+	var mySchedule = new Schedule({
+		el: '#schedule-box',
+		//date: '2018-9-20',
+		clickCb: function (y,m,d) {
+			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		nextMonthCb: function (y,m,d) {
+			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		nextYeayCb: function (y,m,d) {
+			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		prevMonthCb: function (y,m,d) {
+			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		prevYearCb: function (y,m,d) {
+			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		}
+	});
+    //初始化日历
+    $(".new_task_date").on("click",function(e){
+        $("#schedule-box").show();
+    })
     });
     return exports;
     }());
