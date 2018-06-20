0d805f468de26462bef75e7000260888ad84a38f
zyc <zg-zyc@zg18.com>
2018-06-20 20:05:20 +0800

    feat:待办事项日期显示

--- static/js/message_fetch.js
+++ static/js/message_fetch.js
@@ -159,10 +159,20 @@ exports.load_messages = function (opts) {
                                 contentType:"application/json",
                                 data:obj,
                                 success:function(res){
+                                    function timestampToTime(timestamp) {
+                                        var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+                                        Y = date.getFullYear() + '-';
+                                        M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+                                        D = date.getDate() + ' ';
+                                        h = date.getHours() + ':';
+                                        m = date.getMinutes() + ':';
+                                        s = date.getSeconds();
+                                        return Y+M+D+h+m+s;
+                                    }
                                     var taskdetail_list = res.backlogs_dict.task;
                                     var taskdetail_addnote = res.backlogs_dict.task_details;
-                                    var create_time = res.backlogs_dict;
-                                    var over_time = res.backlogs_dict.over_time;
+                                    var create_time = timestampToTime(res.backlogs_dict.create_time).substring(0,10);
+                                    var over_time = timestampToTime(res.backlogs_dict.over_time).substring(0,10);
                                     var state = res.backlogs_dict.state;
                                     var html = templates.render("taskdetail_md",{
                                         taskdetail_list:taskdetail_list,
