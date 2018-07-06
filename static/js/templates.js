ef81fdabe14eaf042c438a8846ff86165669988d
zyc <zg-zyc@zg18.com>
2018-07-06 13:40:50 +0800

    style:添加附件图标，按类显示

--- static/js/templates.js
+++ static/js/templates.js
@@ -114,6 +114,43 @@ Handlebars.registerHelper("substr",function(str){
     var s = str.substring(0,10);
     return s;
 })
+//文件类型判断
+Handlebars.registerHelper("isJpg",function(str){
+    if(str == "jpg"){
+        return true;
+    }else{
+        return false;
+    }
+})
+Handlebars.registerHelper('compare', function(left, operator, right, options) {
+    if (arguments.length < 3) {
+      throw new Error('Handlerbars Helper "compare" needs 2 parameters');
+    }
+    var operators = {
+      '==':     function(l, r) {return l == r; },
+      '===':    function(l, r) {return l === r; },
+      '!=':     function(l, r) {return l != r; },
+      '!==':    function(l, r) {return l !== r; },
+      '<':      function(l, r) {return l < r; },
+      '>':      function(l, r) {return l > r; },
+      '<=':     function(l, r) {return l <= r; },
+      '>=':     function(l, r) {return l >= r; },
+      'typeof': function(l, r) {return typeof l == r; }
+    };
+
+    if (!operators[operator]) {
+      throw new Error('Handlerbars Helper "compare" doesn\'t know the operator ' + operator);
+    }
+
+    var result = operators[operator](left, right);
+
+    if (result) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+});
+
 Handlebars.registerHelper('if_or', function () {
     // Execute the conditional code if any of the conditions are true.
     // Example usage:
