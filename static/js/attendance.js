e594d756d486bf05c896b7a1e54d8a98b75831ff
zyc <zg-zyc@zg18.com>
2018-07-11 16:40:35 +0800

    style:添加考勤文件

---
+++ static/js/attendance.js
@@ -0,0 +1,16 @@
+var attendance = (function () {
+    var exports = {};
+  
+    console.log("这是一21312天1231123123122")
+    alert("风儿那么缠绵")
+    var html = templates.render("attendance_box")
+    console.log($(".generate_log"))
+    var s = document.getElementById("#panels")
+    $(".app").after(html)
+    console.log(s)
+    return exports;
+    }());
+    
+    if (typeof module !== 'undefined') {
+        module.exports = attendance ;
+    }
