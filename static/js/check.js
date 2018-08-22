c63465b2503c8913e49de46cd3bc0f3e06a6d7a9
zg xy <zg-ty@zg18.com>
2018-08-22 11:23:38 +0800

    出差的接口基本上完成，待优化

--- static/js/check.js
+++ static/js/check.js
@@ -5,6 +5,34 @@ var check = (function () {
         var li = templates.render("tab")
         $(".move_ctn").html(li)
     }
+    function backIcon(){
+        $(".first-icon").on("click",function(e){
+            moveContent()
+        })
+    }
+    function commonContent(type) {
+        var content = $.trim($("#username").val())
+        // 开始时间
+          var start_time =$(".start_times").val()
+          var end_time = $(".end_times").val()
+          var count = Number($("#email").val())
+          var cause = $("#text").val()
+          var send_list =[]
+        $(".check-people").children().not($(".add_log_people")).each(function(){
+            var ids= Number($(this).attr('data_id'))
+            send_list.push(ids)
+        })
+        var  data = {
+            approval_type:type,
+            approver_list:send_list,
+            content:content,
+            start_time:start_time,
+            end_time:end_time,
+            count:count,
+            cause:cause
+        }
+        return data
+    }
     $("body").ready(function(){
        $(".notice_box").on("click",".common_check",function(e){
             $(this).addClass("backgr").siblings().removeClass("backgr")
@@ -85,27 +113,43 @@ var check = (function () {
             })
             $("#btn-test").on("click",function(e){
                  e.preventDefault()
-                 var content = $.trim($("#username").val())
-                // 开始时间
-                  var start_time =$(".start_times").val()
-                  var end_time = $(".end_times").val()
-                  var count = Number($("#email").val())
-                  var cause = $("#text").val()
-                  var send_list =[]
-                $(".check-people").children().not($(".add_log_people")).each(function(){
-                    var ids= Number($(this).attr('data_id'))
-                    send_list.push(ids)
-                })
-                console.log(send_list)
-                var  data = {
-                    approval_type:"leave",
-                    approver_list:send_list,
-                    content:content,
-                    start_time:start_time,
-                    end_time:end_time,
-                    count:count,
-                    cause:cause
-                }
+                 var data = commonContent('leave')
+                 channel.post({
+                     url:"/json/zg/approval/leave/",
+                     data:JSON.stringify(data),
+                     contentType:"application/json",
+                     success:function(data){
+                         if(data.errno===0){
+                            moveContent()
+                         }
+                     }
+                 })
+            })
+            backIcon()
+        })
+        $(".move_ctn").on("click",".on_business_trip",function(e){
+            $(".move_ctn").children().remove();
+            var li = templates.render("ask-for-leave",{show:true})
+            $(".move_ctn").html(li) 
+            backIcon()
+            $('#newplan_datetimepicker2').datetimepicker({  
+                language:"zh-CN",  
+                todayHighlight: true,  
+                minView:2,//最精准的时间选择为日期0-分 1-时 2-日 3-月  
+                weekStart:1  
+            }); 
+            $('#newplan_datetimepicker1').datetimepicker({  
+                language:"zh-CN",  
+                todayHighlight: true,  
+                minView:2,//最精准的时间选择为日期0-分 1-时 2-日 3-月  
+                weekStart:1  
+            });
+            $(".add_log_people").on("click",function(e){
+                attendance.peopleChoose(confirm)
+            })
+            $("#btn-test").on("click",function(e){
+                 e.preventDefault()
+                 var data = commonContent("evection")
                  channel.post({
                      url:"/json/zg/approval/leave/",
                      data:JSON.stringify(data),
@@ -118,7 +162,6 @@ var check = (function () {
                  })
             })
         })
-        
     })
     return exports;
 }());
