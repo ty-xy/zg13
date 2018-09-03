a33cc11f6ad1fada8c207932d3001a58d3385468
zg xy <zg-ty@zg18.com>
2018-09-03 11:04:21 +0800

    添加修改群组的js的组件

--- static/js/choose_file.js
+++ static/js/choose_file.js
@@ -1,24 +1,10 @@
 var choose_file = (function () {
     var exports = {};
-    // try_up.feature_check($("#compose #attach_files"));
-    // upload_try.feature_check($("#file-choose #attach_file"));
-    $("#file-choose").on("click", "#attach_files", function (e) {
-        // e.preventDefault();
-        $("#file-choose #file_inputs").trigger("click");
-
-    });
-    // 2  // Click event binding for "Attach files" button
-    // // Triggers a click on a hidden file input field
-    // $("#compose").filedrop(
-    //     try_up.options({
-    //         mode: 'choose',
-    //     })
-    // );
-    // $("#compose").on("click", "#attach_files", function (e) {
-    //     e.preventDefault();
-    //     $("#compose #file_input").trigger("click");
-    // });
-  
+    $(function (){
+       //选择群组
+       
+       
+    })
     return exports;
 }());
 if (typeof module !== 'undefined') {
