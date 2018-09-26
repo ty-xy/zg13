6b25b839d31c0acdad110774abec8b3ea61e8c67
ZG-LXY <zg-lxy@zg18.com>
2018-09-26 10:19:33 +0800

    日至详情bug

--- build/lib/zerver/lib/url_preview/parsers/__init__.py
+++
@@ -1,4 +0,0 @@
-from zerver.lib.url_preview.parsers.open_graph import OpenGraphParser
-from zerver.lib.url_preview.parsers.generic import GenericParser
-
-__all__ = ['OpenGraphParser', 'GenericParser']
